e85537ac75009ab38561b7b1190662cb324974ee
melonholy <36301981+melonholy@users.noreply.github.com>
2019-08-13 15:23:54 +0300

    Footer ready

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Footer from './footer/index';
+import styles from './styles.module.scss';
 
 const App = () => {
   return (
-    <div>
-      <Footer />;
+    <div className={styles.page}>
+      <Footer />
     </div>
   );
 };
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
+
+import { FaVk, FaYoutube, FaFacebookSquare } from 'react-icons/fa';
 import styles from './styles.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>adsa</div>
-      <div>adsda</div>
+      <div>
+        <img src="src\assets\logoLight.png" alt="" />
+        <p>Description</p>
+        <p>© 2019 :iTechArt All Rights Reserved.</p>
+      </div>
+      <div className={styles.social_media_container}>
+        <h2>Follow us on</h2>
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://vk.com/itechart.group">
+            <span className={styles.social_media}>
+              <FaVk />
+            </span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/user/iTechArt">
+            <span className={styles.social_media}>
+              <FaYoutube />
+            </span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/iTechArt.Group/">
+            <span className={styles.social_media}>
+              <FaFacebookSquare />
+            </span>
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
-
