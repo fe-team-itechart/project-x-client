fc69511d75078a85fb84a12f7633fa28d648942e
Siarhei Vei <Siarhei.Vei@itechart-group.com>
2019-08-14 10:51:57 +0300

    rewrite

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Profile from '../containers/profile/profile.jsx';
 
 const App = () => {
   return (
-    <div>aaa</div>
+    <div>
+      <p>test app</p>
+      <Profile />
+    </div>
   );
 };
 
-export default App
+export default App;
