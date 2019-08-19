62901f252e51a14a6157cb77b38d80f594753a68
slavasgod <slavasgod@gmail.com>
2019-08-19 13:52:50 +0300

    main page container

---
+++ src/containers/mainPage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import styles from './styles.module.scss';
+
+export default class MainPage extends Component {
+  state = {};
+
+  render() {
+    return <main className={styles.main_page_wrapper}>MainPage</main>;
+  }
+}
