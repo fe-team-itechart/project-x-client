2f1a8126ab7997e1b08da0194fb53d099f26f627
Artsiom.Kashyn <Artsiom.Kashyn@itechart-group.com>
2019-08-19 11:00:10 +0300

    fix unused imports

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Register from '../containers/auth/register';
-import Login from '../containers/auth/login';
 
 import { Profile } from '../containers/profile/';
 import Header from '../containers/header';
