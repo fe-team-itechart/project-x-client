68ad86f6f5eb2a90ca8000ee6400f8791672fb88
slavasgod <slavasgod@gmail.com>
2019-08-20 09:09:25 +0300

    connected with redux

--- src/containers/mainPage/index.js
+++ src/containers/mainPage/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import styles from './styles.module.scss';
 
-export default class MainPage extends Component {
+class MainPage extends Component {
   state = {};
 
   render() {
     return <main className={styles.main_page_wrapper}>MainPage</main>;
   }
 }
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainPage);
