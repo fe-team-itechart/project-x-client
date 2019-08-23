7af6bda86e5a6bc67fb95e248514bec50db526e1
Siarhei Vei <Siarhei.Vei@itechart-group.com>
2019-08-23 13:52:20 +0300

    conflicts with routers resolved

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Layout } from '../layout';
 import { GettingStarted } from '../help/gettingStarted';
-import { Profile } from '../../containers/profile';
+import { ProfileRouters } from '../../containers/profile';
 import { Trobleshooting } from '../help/troubleshooting';
 import { CourseTaking } from '../help/courseTaking'
 
@@ -12,7 +12,7 @@ export const Main = () => {
     <Switch>
       <Route exact path="/" component={Layout} />
       <Route path="/getting-started" component={GettingStarted} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/profile" component={ProfileRouters} />
       <Route path="/troubleshooting" component={Trobleshooting} />
       <Route path="/course-taking" component={CourseTaking} />
     </Switch>
