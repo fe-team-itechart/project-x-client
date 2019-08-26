9cdd8d06283e1fd09af9359ca61b1b3b55f35d5a
Oleg <oleg.panasyuk2@inbox.ru>
2019-08-26 17:23:31 +0300

    Add validation to forms

--- src/containers/auth/forgotPassword/index.jsx
+++ src/containers/auth/forgotPassword/index.jsx
@@ -9,6 +9,8 @@ import { IoIosCheckmarkCircle } from 'react-icons/io';
 
 import styles from './styles.module.scss';
 import { forgotPasswordRequest } from '../../../services/auth';
+import validateAuth from '../../../validation/auth';
+import Spinner from '../../../components/spinner/index';
 
 Modal.setAppElement('#root');
 
@@ -16,26 +18,53 @@ class ForgotPassword extends PureComponent {
   state = {
     status: null,
     data: null,
-    email: null
-  }
+    email: null,
+    message: null,
+    pending: false,
+  };
 
   refInput = React.createRef();
 
-  send = async (email) => {
-    const response = await forgotPasswordRequest({ email });
-    const { data, status } = response;
+  send = async email => {
+    const errors = validateAuth({ email });
     this.setState({
+      pending: true,
+    });
+    const response = !errors.email && (await forgotPasswordRequest({ email }));
+    const { data, status } = !errors.email && response;
+    let answer =
+      !errors.email &&
+      this.setState({
         status,
-        data
-      })
-  }
+        data,
+      });
+    answer =
+      errors.email &&
+      this.setState({
+        message: errors.email,
+      });
+    this.setState({
+      pending: false,
+    });
+    return answer;
+  };
 
   onChangeInput = () => {
     const email = this.refInput.current.value;
     this.setState({
-      email
+      email,
     });
-  } 
+  };
+
+  onCloseModalSetDefault = () => {
+    this.setState({
+      status: null,
+      data: null,
+      email: null,
+      message: null,
+      pending: false,
+    });
+  };
 
   render() {
     const {
@@ -48,68 +77,85 @@ class ForgotPassword extends PureComponent {
       rowTop,
       closeModal,
       successMessage,
-      errorMessage
+      errorMessage,
+      big_size_message,
     } = styles;
 
-    const {
-      status,
-      data,
-      email
-    } = this.state;
+    const { status, data, email, message, pending } = this.state;
 
-    const {
-      modalStatus,
-      onModalClose
-    } = this.props;
+    const { modalStatus, onModalClose } = this.props;
 
     return (
       <Modal
         isOpen={modalStatus}
         onAfterOpen={this.afterOpenModal}
-        onRequestClose={() => { onModalClose(false); }}
-        className={modal}
-      >
-        <FaTimes onClick={() => { onModalClose(false); }} className={closeModal} />
+        onRequestClose={() => {
+          onModalClose(false);
+        }}
+        className={modal}>
+        <FaTimes
+          onClick={() => {
+            this.onCloseModalSetDefault();
+            onModalClose(false);
+          }}
+          className={closeModal}
+        />
         <form className={form}>
-          {(!status) && (<div className={`${row} ${rowTop}`}>
-            <input
-              type="email"
-              name="forgotPassWordEmail"
-              id="forgotPassWordEmail"
-              className={input}
-              ref={this.refInput}
-              placeholder='Enter email'
-              onChange={this.onChangeInput}
-            />
-          </div>)
-          }
+          {(!status) && (!pending) && (
+            <div className={`${row} ${rowTop}`}>
+              <input
+                type="email"
+                name="forgotPassWordEmail"
+                id="forgotPassWordEmail"
+                className={input}
+                ref={this.refInput}
+                placeholder="Enter email"
+                onChange={this.onChangeInput}
+                onInput={() =>
+                  this.setState({
+                    message: null,
+                  })
+                }
+                required
+              />
+              <br />
+              <p className={`${errorMessage} `}>{!!message && message}</p>
+            </div>
+          )}
           {
-            (status < 300) && (
-            <>
-              <div className={`${row} ${rowTop} ${successMessage}`}>
-                <IoIosCheckmarkCircle hidden={!status}/>{data}
+            (pending) && (
+              <div className={`${row} ${rowTop}`}>
+                <Spinner />
               </div>
-            </>
             )
           }
-          {
-            (status >= 300) && (
-              <>
-                <div className={`${row} ${rowTop} ${errorMessage}`}>
-                  {data}
-                </div>
-              </>
-              )
-          }
+          {status < 300 && (
+            <>
+              <div className={`${row} ${rowTop} ${successMessage} ${big_size_message}`}>
+                <IoIosCheckmarkCircle hidden={!status} />
+                {data}
+              </div>
+            </>
+          )}
+          {status >= 300 && (
+            <>
+              <div className={`${row} ${rowTop} ${errorMessage} ${big_size_message}`}>{data}</div>
+            </>
+          )}
           <div className={`${row} ${rowFlexEnd}`}>
-            <button type='button' className={btn} onClick={() => { this.send(email) }} >Push</button>
+            <button
+              type="submit"
+              className={btn}
+              onClick={e => {
+                e.preventDefault();
+                this.send(email);
+              }}>
+              Push
+            </button>
           </div>
-
         </form>
-
       </Modal>
     );
-
   }
 }
 
--- src/containers/auth/resetPassword/index.jsx
+++ src/containers/auth/resetPassword/index.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import styles from './styles.module.scss';
 import { resetApprove, resetPassword } from '../../../services/auth';
+import Spinner from '../../../components/spinner/index';
+import validateAuth from '../../../validation/auth';
 
 class ResetPassword extends PureComponent {
     state = {
@@ -8,7 +10,8 @@ class ResetPassword extends PureComponent {
         status: null,
         password: null,
         passwordConfirm: null,
-        linkId: null
+        linkId: null,
+        message: null
     }
 
     componentDidMount() {
@@ -17,24 +20,52 @@ class ResetPassword extends PureComponent {
     }
 
     sendApprove = async (linkId) => {
-        const { status } = await resetApprove(linkId);
-        this.setState({
-            status,
-            linkId
-        });
-    }
-
-    sendReset = async () => {
-        const { linkId, password, passwordConfirm } = this.state;
-        
-        if (password && passwordConfirm) {
-            const { status } = await resetPassword({ linkId, password, passwordConfirm });
+        const { status, data } = await resetApprove(linkId);
+        if (data.name === 'Error') {
             this.setState({
-                status,
-                success: (status < 300)
+                status: 400,
+                linkId: null,
             });
         } else {
-            console.log('enter passwords');
+            this.setState({
+                status,
+                linkId
+            });
+        }
+    }
+
+    sendReset = async (e) => {
+        e.preventDefault();
+        const { linkId, password, passwordConfirm } = this.state;
+        const errors = validateAuth({ password });
+        if (password && passwordConfirm) {
+            if (password === passwordConfirm) {
+                if (errors.password) {
+                    this.setState({
+                        message: errors.password
+                    });
+                } else {
+                    const { status, data } = await resetPassword({ linkId, password, passwordConfirm });
+                    if (data.status === 200) {
+                        this.setState({
+                            status,
+                            success: (status < 300)
+                        });
+                    } else {
+                        this.setState({
+                            message: data.message
+                        });
+                    }
+                }
+            } else {
+                this.setState({
+                    message: 'Passwords not equal'
+                });
+            }
+        } else {
+            this.setState({
+                message: 'Enter password'
+            });
         }
 
 
@@ -47,14 +78,17 @@ class ResetPassword extends PureComponent {
     }
 
     render() {
-        const { status, success } = this.state;
+        const { status, success, message } = this.state;
         const {
             input,
             btn,
             form,
             row,
             rowFlexEnd,
-            wrapperF
+            wrapperF,
+            errorMessage,
+            successMessage,
+            big_size_message,
         } = styles;
 
         return (
@@ -65,32 +99,36 @@ class ResetPassword extends PureComponent {
                         className={form}
                     >
                         <div className={row}>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder='Enter Password'
-                            onChange={this.inputHandler}
-                            className={input}
-                        />
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder='Enter Password'
+                                onChange={this.inputHandler}
+                                className={input}
+                                required
+                            />
                         </div>
                         <div className={row}>
-                        <input
-                            type="password"
-                            name="passwordConfirm"
-                            id="passwordConfirm"
-                            placeholder='Confirm Password'
-                            onChange={this.inputHandler}
-                            className={input}
-                        />
+                            <input
+                                type="password"
+                                name="passwordConfirm"
+                                id="passwordConfirm"
+                                placeholder='Confirm Password'
+                                onChange={this.inputHandler}
+                                className={input}
+                                required
+                            />
                         </div>
+                        <p className={`${errorMessage} `}>&nbsp;{!!message && message}</p>
                         <div className={`${row} ${rowFlexEnd}`}>
-                            <button type='button' className={btn} onClick={this.sendReset} >Push</button>
+                            <button type='submit' className={btn} onClick={this.sendReset} >Push</button>
                         </div>
+
                     </form>)}
-                {(status) && (!success) && (status > 300) && (<div>Something is wrong</div>)}
-                {(!status) && (!success) && (<div>Checking link</div>)}
-                {(success) && (<div>Password is Updated</div>)}
+                {(status) && (!success) && (status > 300) && (<div className={`${big_size_message} ${errorMessage}`}>Something is wrong</div>)}
+                {(!status) && (!success) && (<Spinner />)}
+                {(success) && (<div className={`${big_size_message} ${successMessage}`}>Password is Updated</div>)}
             </div>
         );
     }
