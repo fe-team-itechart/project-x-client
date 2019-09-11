c84e744d94af2b0235b28c86d1e21d0368b143cf
Artsiom.Kashyn <Artsiom.Kashyn@itechart-group.com>
2019-09-11 10:40:29 +0300

    deleted trash

--- src/services/auth.js
+++ src/services/auth.js
@@ -38,17 +38,6 @@ export const loginRequest = async data => {
   return decoded;
 };
 
-export const googleLoginRequest = async data => {
-  const {
-    data: { token },
-  } = await httpService.post({ url: 'users/login', data, config });
-  const decoded = jwt_decode(token);
-  localStorage.setItem('token', token);
-  setAuthToken(token);
-
-  return decoded;
-};
-
 export const changePassword = async (id, data) => {
   const res = await httpService.put({ url: 'users/change-password', id, data });
   return res;
