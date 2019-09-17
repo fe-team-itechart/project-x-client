05e581a090d0d121e4058e2f2ff27584d2c6b3b4
melonholy <36301981+melonholy@users.noreply.github.com>
2019-09-17 09:44:21 +0300

    carousel ready

--- src/containers/couresCarousel/index.js
+++ src/containers/couresCarousel/index.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AliceCarousel from 'react-alice-carousel';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 import 'react-alice-carousel/lib/alice-carousel.css';
 import styles from './styles.module.scss';
 
 class CoursesCarousel extends Component {
-  state = { 
-    currentIndex: 0 
-  };
+  state = {};
 
   galleryItems = () => {
     return Array(12)
       .fill()
       .map((item, i) => (
         <div className={styles.course_card_container}>
+          <button type="button" className={styles.adding_button}>
+            <span>+</span>
+          </button>
           <figure className={styles.image} />
           <p className={styles.name}>React js</p>
           <p className={styles.duration}>Duration: 35d {i}</p>
@@ -25,23 +27,37 @@ class CoursesCarousel extends Component {
 
   nextItem = () => {
     this.Carousel.slideNext();
-  }
-  
+  };
+
   prevItem = () => {
     this.Carousel.slidePrev();
-  }
+  };
 
   render() {
     const responsive = {
-      1024: {
+      1650: {
         items: 5,
+      },
+      1430: {
+        items: 4,
+      },
+      1070: {
+        items: 3,
+      },
+      730: {
+        items: 2,
       },
     };
 
     return (
       <section className={styles.carousel_container}>
-        <button style={{width: '5%', float: 'left'}} type='button' onClick={this.prevItem}>Prev button</button>
-        <div style={{width: '90%', float: 'left'}}>
+        <button
+          className={styles.nav_button}
+          type="button"
+          onClick={this.prevItem}>
+          <IoIosArrowBack color="white" size="large" />
+        </button>
+        <div className={styles.carousel}>
           <AliceCarousel
             items={this.galleryItems()}
             mouseDragEnabled
@@ -49,10 +65,15 @@ class CoursesCarousel extends Component {
             responsive={responsive}
             dotsDisabled
             buttonsDisabled
-            ref={(el) => (this.Carousel = el)}
+            ref={el => (this.Carousel = el)}
           />
         </div>
-        <button style={{width: '5%', float: 'left'}} type='button' onClick={this.nextItem}>Next button</button>
+        <button
+          className={styles.nav_button}
+          type="button"
+          onClick={this.nextItem}>
+          <IoIosArrowForward color="white" size="large" />
+        </button>
       </section>
     );
   }
