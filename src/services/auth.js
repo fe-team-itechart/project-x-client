792ed11b9a29cc1fc40b775e7be44bbaccb5a540
Artsiom.Kashyn <Artsiom.Kashyn@itechart-group.com>
2019-09-20 12:51:22 +0300

    bug was fixed

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,7 +5,6 @@ import { httpService } from './httpService';
 
 const config = {
   headers: { 'Content-Type': 'application/json' },
-  Authorization: localStorage.token,
 };
 
 export const socialLoginRequest = res => {
