eca1b059623c32124d19ac9acadb9e44b2829bc9
Oleg <oleg.panasyuk2@inbox.ru>
2019-10-16 16:24:53 +0300

    Adding additional functionality for rating.

--- src/components/courseCard/index.js
+++ src/components/courseCard/index.js
@@ -1,49 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from '../rating/index';
 
 import styles from './style.module.scss';
-import { Redirect } from 'react-router-dom';
+
 import { links } from '../../utils/constants';
+import Img from '../img/index';
 
 export default function CourseCard({ item }) {
-  const { title, author, rate, price, img, id = 1, votes } = item;
-  const [redirect, setRedirect] = useState(false);
-  const redirection = e => {
-    e.preventDefault();
-    e.stopPropagation();
-    setRedirect(true);
-  };
+  const { title, author, rate, price, img = '', id = 1, votes } = item;
 
-  const handlerRating = e => {
-    e.stopPropagation();
-    if (e.currentTarget.tagName === 'LABEL') {
-      return false;
-    }
-    // the place for onClick rating Handler.
-  };
-
-  return !redirect ? (
-    <figure className={styles.courseCard} onClick={redirection}>
-      <img
-        src={img}
-        alt="Course Preview"
-        className={styles.courseCardPreview}
-      />
-      <section className={styles.courseCardDescription}>
-        <h2 className={styles.courseCardTitle}>{title}</h2>
-        <div className={styles.courseCardAuthor}>{author}</div>
-        <div className={styles.courseCardRate}>
-          <Rating rate={rate} votes={votes} handler={handlerRating} />
-        </div>
-        <div className={styles.courseCardPrice}>{price}</div>
-      </section>
-    </figure>
-  ) : (
-    <Redirect
-      push
-      to={{
-        pathname: `${links.coursePreview}${id}`,
-      }}
-    />
+  return (
+    <Link to={`${links.coursePreview}${id}`} className={styles.courseCardWrapper}>
+      <figure className={styles.courseCard} >
+        <Img
+          src={img}
+          alt="Course Preview"
+          className={styles.courseCardPreview}
+        />
+        <section className={styles.courseCardDescription}>
+          <h2 className={styles.courseCardTitle}>{title}</h2>
+          <div className={styles.courseCardAuthor}>{author}</div>
+          <div className={styles.courseCardRate}>
+            <Rating rate={rate} votes={votes}/>
+          </div>
+          <div className={styles.courseCardPrice}>{price}</div>
+        </section>
+      </figure>
+    </Link>
   );
 }
--- src/components/rating/index.js
+++ src/components/rating/index.js
@@ -3,46 +3,60 @@ import { FaStar } from 'react-icons/fa';
 
 import styles from './style.module.scss';
 
-export default function Rating({ rate = 0, handler, votes = 100 }) {
-  if (!handler) {
-    handler = e => {
-      e.stopPropagation();
-      if (e.currentTarget.tagName === 'LABEL') {
-        return false;
-      }
-      console.log(e.currentTarget.id);
-    };
+const handlerDefault = e => {
+  e.stopPropagation();
+  if (e.currentTarget.tagName === 'LABEL') {
+    return false;
+  }
+  console.log('Default clicked')
+};
+
+export default function Rating({ rate = 0, handler = handlerDefault, votes = 100, voteAble = false}) {
+  let handlerPrepared;
+  if (!voteAble) {
+    handlerPrepared = (e) => {
+      e.preventDefault();
+    }
+  } else {
+    handlerPrepared = handler;
   }
 
-  rate = Math.round(rate);
+  const instanceId = Math.round(Math.random() * 100000);
+
+  const rateRounded = Math.round(rate);
 
   const rateStars = Array(6)
     .fill(1)
-    .map((el, i) => (
-      <React.Fragment key={`rating-${i}`}>
+    .map((el, i) => {
+      const k = Math.random();
+      return (
+      <React.Fragment key={`rating-${k}`}>
         <label
-          htmlFor={`rating-${i}`}
+          htmlFor={`rating-${k}`}
           className={styles.inputRateLabel}
-          onClick={handler}
+          onClick={handlerPrepared}
           style={{ display: i == 0 ? 'none' : 'block' }}>
           <FaStar className={`${styles.starRate}`} />
         </label>
         <input
           type="radio"
-          name="rating"
-          id={`rating-${i}`}
+          name={`rating-${instanceId}`}
+          id={`rating-${k}`}
           className={styles.inputRateRatio}
-          onClick={handler}
+          onClick={handlerPrepared}
           value={i}
-          defaultChecked={i === rate}
+          defaultChecked={i === rateRounded}
         />
       </React.Fragment>
-    ));
+    )});
 
+  const classForGroup = `${voteAble && styles.rateGroupVoteAble} ${styles.rateGroup}`;
   return (
-    <div className={styles.rateGroup}>
+    
+    <div className={classForGroup}>
       {rateStars}
       <span className={styles.rateVotes}>({votes})</span>
     </div>
+    
   );
 }
