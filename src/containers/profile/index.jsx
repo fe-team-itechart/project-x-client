3060fea228fb80b3cee5fce84712ffd14935908e
Viachaslau <slavasgod@gmail.com>
2019-10-24 17:33:48 +0300

    fixed profile markup, added ability to change emai

--- src/containers/profile/index.jsx
+++ src/containers/profile/index.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import { PrivateRoute } from '../../components/privateRoute';
 import ProfileData from './profile/index';
-import { Settings } from './settings';
 import Account from './account';
-import { Courses } from './courses';
+import Profile from './profile';
 
 import styles from './styles.module.scss';
 
-export class ProfileRouters extends Component {
-  render() {
-    return (
-      <section className={styles.profile}>
-        <Switch>
-          <PrivateRoute path="/profile-public" component={ProfileData} />
-          <PrivateRoute path="/profile-courses" component={Courses} />
-          <PrivateRoute path="/profile-account" component={Account} />
-          <PrivateRoute path="/profile-settings" component={Settings} />
-        </Switch>
+export const ProfileRouters = () => {
+  return (
+    <>
+      <section className={styles.profileWrapper}>
+        <Profile />
+        <section className={styles.publicWrapper}>
+          <Switch>
+            <PrivateRoute path="/profile-public" component={ProfileData} />
+            <PrivateRoute path="/profile-account" component={Account} />
+          </Switch>
+        </section>
       </section>
-    );
-  }
-}
+    </>
+  );
+};
